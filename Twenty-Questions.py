f2575673bd05b73d5ae20a69c0c4e944e1c434eb
Amir <65414622+Iam-ho@users.noreply.github.com>
2023-04-10 20:52:13 +0330

    Updated beginner difficulty questions.

--- Twenty-Questions.py
+++ Twenty-Questions.py
@@ -7,3 +7,28 @@ if difficalty == "beginner":
         print("Correct!")
     elif question1 != "dog":
         print("No, The correct answer is dog.")
+        question2 = input("2nd.: What is the meaning of www in url? ")
+    if question2 == "world wide web":
+        print("Correct!!")
+    elif question2 != "world wide web":
+        print("No, The Correct answer is world wide web.")
+    question3 = input("3rd.: How many dots does the dies have? ")
+    if question3 == "21":
+        print("Correct!!!")
+    elif question3 != "21":
+        print("No, The correct answer is 21.")
+    question4 = input("4th.: How many kilometers is the distance of the marathon race? ")
+    if question4 == "42.195":
+        print("correct!!!!")
+    elif question4 != "42.195":
+        print("No, The correct answer is 42.195")
+    question5 = input("5th.: What is the Currency of China? ")
+    if question5 == "yuan":
+        print("Correct!!!!!")
+    elif question5 != "yuan":
+        print("No, The correct answer is yuan.")
+    question6 = input("6th.: What is the name of the Russian spy agency? ")
+    if question6 == "kgb":
+        print("Correct!!!!!!")
+    elif question6 != "kgb":
+        print("No, The correct answer is kgb.")
