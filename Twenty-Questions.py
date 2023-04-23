ff8cba932bd07e0e51c10712f057753d882675b3
Amir <65414622+Iam-ho@users.noreply.github.com>
2023-04-23 13:20:07 +0330

    Update Twenty-Questions.py

--- Twenty-Questions.py
+++ Twenty-Questions.py
@@ -1,34 +1,118 @@
 difficalty = input("Choose the difficulty level; Beginner, Medium, Hard? ")
 
 if difficalty == "beginner":
-    print("Here we go!!!!")
-    question1 = input("1st.: What is the first animal domesticated by humans? ")
+    print("Ok, Here we go!!!!")
+    question1 = input("1: What is the first animal domesticated by humans? ")
     if question1 == "dog":
-        print("Correct!")
+        print("Correct!!")
     elif question1 != "dog":
         print("No, The correct answer is dog.")
-        question2 = input("2nd.: What is the meaning of www in url? ")
+    question2 = input("2: What is the meaning of www in url? ")
     if question2 == "world wide web":
         print("Correct!!")
     elif question2 != "world wide web":
         print("No, The Correct answer is world wide web.")
-    question3 = input("3rd.: How many dots does the dies have? ")
+    question3 = input("3: How many dots does the dies have? ")
     if question3 == "21":
-        print("Correct!!!")
+        print("Correct!!")
     elif question3 != "21":
         print("No, The correct answer is 21.")
-    question4 = input("4th.: How many kilometers is the distance of the marathon race? ")
+    question4 = input("4: How many kilometers is the distance of the marathon race? ")
     if question4 == "42.195":
-        print("correct!!!!")
+        print("correct!!")
     elif question4 != "42.195":
         print("No, The correct answer is 42.195")
-    question5 = input("5th.: What is the Currency of China? ")
+    question5 = input("5: What is the Currency of China? ")
     if question5 == "yuan":
-        print("Correct!!!!!")
+        print("Correct!!")
     elif question5 != "yuan":
         print("No, The correct answer is yuan.")
-    question6 = input("6th.: What is the name of the Russian spy agency? ")
+    question6 = input("6: What is the name of the Russian spy agency? ")
     if question6 == "kgb":
-        print("Correct!!!!!!")
+        print("Correct!!")
     elif question6 != "kgb":
         print("No, The correct answer is kgb.")
+    question7 = input("7: In which city is Ferdowsi's tomb? ")
+    if question7 == "tus":
+        print("Correct!!")
+    elif question7 != "tus":
+        print("No, The correct answer is tus.")
+    question8 = input("8: which is the largest country in the world? ")
+    if question8 == "russia":
+        print("Correct!!")
+    elif question8 != "russia":
+        print("No, The correct answer is russia.")
+    question9 = input("9: How many squares does the chessboard consist of? ")
+    if question9 == "64":
+        print("Correct!!")
+    elif question9 != "64":
+        print("No, The correct answer is 64.")
+    question10 = input("10: Which animal can recognize itself in the mirror? ")
+    if question10 == "crow":
+        print("Correct!!")
+    elif question10 != "crow":
+        print("No, The correct answer is crow.")
+    question11 = input("11: How much does an elephant weigh?(Kilograms) ")
+    if question11 == "6800":
+        print("Correct!!")
+    elif question11 != "6800":
+        print("No, The correct answer is 6800 kilograms.")
+    question12 = input("12: In which city was Amir Kabir killed? ")
+    if question12 == "kashan":
+        print("Correct!!")
+    elif question12 != "kashan":
+        print("No, The correct answer is kashan.")
+    question13 = input("13: which is the longest river in the world? ")
+    if question13 == "nile":
+        print("Correct!!")
+    elif question13 != "nile":
+        print("No, The correct answer is nile.")
+    question14 = input("14: which is the largest rainforest in the world? ")
+    if question14 == "amazon":
+        print("Correct!!")
+    elif question14 != "amazon":
+        print("No, The correct answer is amazon.")
+    question15 = input("15: Which continent is the green continent? ")
+    if question15 == "europe":
+        print("Correct!!")
+    elif question15 != "europe":
+        print("No, The correct answer is europe.")
+    question16 = input("16: Which part of the body has no blood at all? ")
+    if question16 == "retina":
+        print("Correct!!")
+    elif question16 != "retina":
+        print("No, The correct answer is retina.")
+    question17 = input("17: Convert 12 cm to mm? ")
+    if question17 == "120 mm":
+        print("Correct!!")
+    elif question17 != "120 mm":
+        print("No, The correct answer is 120 mm.")
+    question18 = input("18: How many planets do we have in our solar system? ")
+    if question18 == "8":
+        print("Correct!!")
+    elif question18 != "8":
+        print("No, The correct answer is 8.")
+    question19 = input("19: Who is the first woman to win the Fields Medal??? ")
+    if question19 == "Maryam Mirzakhani":
+        print("Correct!!!!!!")
+    elif question19 != "Maryam Mirzakhani":
+        print("No, The correct answer is Maryam Mirzakhani" + U'\u2764' + ".")
+    question20 = input("20: Which animal can be seen on the Porsche logo? ")
+    if question20 == "horse":
+        print("Correct!!")
+    elif question20 != "horse":
+        print("No, The correct answer is horse.")
+    print("\nYou answered 20 questions.")
+
+if difficalty == "medium":
+    print("Ok, Here we go!!!!")
+    question1 = input("1: Which animal is more trainable than dog? ")
+    if question1 == "pig":
+        print("Correct!!!")
+    elif question1 != "pig":
+        print("No, The correct answer is pig.")
+    question2 = input("2: How many minutes does it take for sunlight to reach the earth? ")
+    if question2 == "8.17":
+        print("Correct!!!")
+    elif question2 != "8.17":
+        print("No, The correct answer is 8.17 .")
